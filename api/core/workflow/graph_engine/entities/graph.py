f16b50e0bca829d28cd5838c58a3639f8b8f14b4
Aurora <781487461@qq.com>
2025-04-01 18:30:54 +0800

    chore: 更新工作流并行代码注释

--- api/core/workflow/graph_engine/entities/graph.py
+++ api/core/workflow/graph_engine/entities/graph.py
@@ -148,18 +148,20 @@ class Graph(BaseModel):
         if not root_node_id or root_node_id not in root_node_ids:
             raise ValueError(f"Root node id {root_node_id} not found in the graph")
 
-        # Check whether it is connected to the previous node
+        # 检查图是否存在环，避免循环执行
         cls._check_connected_to_previous_node(route=[root_node_id], edge_mapping=edge_mapping)
 
-        # fetch all node ids from root node
+        # 根据edge_mapping，按照DFS添加节点ID
         node_ids = [root_node_id]
         cls._recursively_add_node_ids(node_ids=node_ids, edge_mapping=edge_mapping, node_id=root_node_id)
 
+        # 获取真正有有效(可达)的node_id->node_config的映射，all_node_id_config_mapping是所有的，node_ids是可达的
         node_id_config_mapping = {node_id: all_node_id_config_mapping[node_id] for node_id in node_ids}
 
         # init parallel mapping
         parallel_mapping: dict[str, GraphParallel] = {}
         node_parallel_mapping: dict[str, str] = {}
+        # 最开始初始化的时候是root_node_id, 此时还没有并行, parent_parallel也为None
         cls._recursively_add_parallels(
             edge_mapping=edge_mapping,
             reverse_edge_mapping=reverse_edge_mapping,
@@ -300,38 +302,44 @@ class Graph(BaseModel):
     ) -> None:
         """
         Recursively add parallel ids
-
+        收集属于并行分支的节点 ID，并将它们分配到适当的并行映射中。
         :param edge_mapping: edge mapping
         :param start_node_id: start from node id
         :param parallel_mapping: parallel mapping
         :param node_parallel_mapping: node parallel mapping
         :param parent_parallel: parent parallel
         """
-        target_node_edges = edge_mapping.get(start_node_id, [])
-        parallel = None
+        target_node_edges = edge_mapping.get(start_node_id, [])  # 获取所有出边
+        parallel = None  # 尚未确定并行，设置为None
         if len(target_node_edges) > 1:
+            # 出边数>1，可能存在并行
             # fetch all node ids in current parallels
-            parallel_branch_node_ids = defaultdict(list)
-            condition_edge_mappings = defaultdict(list)
+            parallel_branch_node_ids = defaultdict(list)  # 存储并行分支节点id
+            condition_edge_mappings = defaultdict(list)  # 存储条件边映射
             for graph_edge in target_node_edges:
+                # 根据run_condition分组，没有 run_condition 的边被归类到“default”键下
                 if graph_edge.run_condition is None:
                     parallel_branch_node_ids["default"].append(graph_edge.target_node_id)
                 else:
+                    # 带有 run_condition 的边按其 condition_hash 分组，使该方法能够处理条件分支。
                     condition_hash = graph_edge.run_condition.hash
                     condition_edge_mappings[condition_hash].append(graph_edge)
 
             for condition_hash, graph_edges in condition_edge_mappings.items():
+                # 如果条件边映射中有多个边，可能存在并行分支，将条件hash作为键，存储对应的目标节点id
                 if len(graph_edges) > 1:
                     for graph_edge in graph_edges:
                         parallel_branch_node_ids[condition_hash].append(graph_edge.target_node_id)
 
             condition_parallels = {}
             for condition_hash, condition_parallel_branch_node_ids in parallel_branch_node_ids.items():
+                # 遍历所有并行分支(分支id -> 目标节点id列表)
                 # any target node id in node_parallel_mapping
                 parallel = None
                 if condition_parallel_branch_node_ids:
                     parent_parallel_id = parent_parallel.id if parent_parallel else None
 
+                    # uuid, 当前的start node id, 父parallel的id, 父parallel的start node id
                     parallel = GraphParallel(
                         start_from_node_id=start_node_id,
                         parent_parallel_id=parent_parallel.id if parent_parallel else None,
@@ -340,6 +348,8 @@ class Graph(BaseModel):
                     parallel_mapping[parallel.id] = parallel
                     condition_parallels[condition_hash] = parallel
 
+                    # 检索并行每个分支中的所有节点, 递归遍历每个分支，收集节点 ID。
+                    # in_branch_node_ids: {branch_node_id: node_ids(不包含合并节点)}
                     in_branch_node_ids = cls._fetch_all_node_ids_in_parallels(
                         edge_mapping=edge_mapping,
                         reverse_edge_mapping=reverse_edge_mapping,
@@ -347,17 +357,23 @@ class Graph(BaseModel):
                     )
 
                     # collect all branches node ids
+                    # 如果节点是父并行的一部分或没有父并行，则将节点包含在当前并行中。
                     parallel_node_ids = []
                     for _, node_ids in in_branch_node_ids.items():
                         for node_id in node_ids:
+                            # 检查当前节点是否已分配给父并行（如果有的话），或者是否可以安全地分配给当前并行。
                             in_parent_parallel = True
                             if parent_parallel_id:
                                 in_parent_parallel = False
                                 for parallel_node_id, parallel_id in node_parallel_mapping.items():
                                     if parallel_id == parent_parallel_id and parallel_node_id == node_id:
+                                        # 当前节点是父级并行的一部分，退出循环。
                                         in_parent_parallel = True
                                         break
 
+                            # 如果当前节点没有父并行节点/如果当前节点存在父并行节点，并且在父并行中
+                            # 说明可以作为当前并行的节点
+                            # 否则如果存在父并行，又不在父并行中，则不作为当前并行的节点
                             if in_parent_parallel:
                                 parallel_node_ids.append(node_id)
                                 node_parallel_mapping[node_id] = parallel.id
@@ -557,12 +573,22 @@ class Graph(BaseModel):
     ) -> dict[str, list[str]]:
         """
         Fetch all node ids in parallels
+        主要用于识别并行分支中的节点以及分支合并点。
         """
+
+        """
+        routes_node_ids : 存储从每个分支起始节点可访问的节点 ID 列表。
+        leaf_node_ids : 存储每个分支的叶节点 ID（没有出边的节点）。
+        merge_branch_node_ids : 存储分支可能重新加入的潜在合并节点。
+        """
+
         routes_node_ids: dict[str, list[str]] = {}
         for parallel_branch_node_id in parallel_branch_node_ids:
             routes_node_ids[parallel_branch_node_id] = [parallel_branch_node_id]
 
             # fetch routes node ids
+            # routes_node_ids[target_node_id] = [target_node_id, accessable node ids...]
+            # 为每个并行分支的起始节点创建一个列表,使用_recursively_fetch_routes收集每个分支中的所有可达节点
             cls._recursively_fetch_routes(
                 edge_mapping=edge_mapping,
                 start_node_id=parallel_branch_node_id,
@@ -573,13 +599,17 @@ class Graph(BaseModel):
         leaf_node_ids: dict[str, list[str]] = {}
         merge_branch_node_ids: dict[str, list[str]] = {}
         for branch_node_id, node_ids in routes_node_ids.items():
+            # 遍历每个分支(分支的起始节点id, 分支上的所有节点id)
             for node_id in node_ids:
+                # 识别叶子节点，叶子节点是不在edge_mapping里的，因为没有出边，在reverse_edge_mapping中有
                 if node_id not in edge_mapping or len(edge_mapping[node_id]) == 0:
+                    # 初始化空列表
                     if branch_node_id not in leaf_node_ids:
                         leaf_node_ids[branch_node_id] = []
 
                     leaf_node_ids[branch_node_id].append(node_id)
 
+                # branch_node_id是分支1, 遍历除了分支1的所有分支(分支2), 如果分支2的路径上存在分支1的相同节点id, 那么就是潜在的合并节点
                 for branch_node_id2, inner_route2 in routes_node_ids.items():
                     if (
                         branch_node_id != branch_node_id2
@@ -591,16 +621,22 @@ class Graph(BaseModel):
                             routes_node_ids=routes_node_ids,
                         )
                     ):
+                        # 初始化空列表，可能有多个潜在的合并的节点(例如两个分支合并后的路径是一条单路径)
                         if node_id not in merge_branch_node_ids:
                             merge_branch_node_ids[node_id] = []
 
+                        # 为什么将分支的节点的id添加到merge_branch_node_ids?  第一个分支一开始是没添加，但遍历到后面的时候，第一个分支实际上就是分支2，分支2变成了分支1，所以最开始的分支也被添加进来了！！！
                         if branch_node_id2 not in merge_branch_node_ids[node_id]:
                             merge_branch_node_ids[node_id].append(branch_node_id2)
 
         # sorted merge_branch_node_ids by branch_node_ids length desc
+        # 按合并分支数量排序合并节点
         merge_branch_node_ids = dict(sorted(merge_branch_node_ids.items(), key=lambda x: len(x[1]), reverse=True))
 
+        # 初始化重复或重叠的合并节点(后面要删除)
         duplicate_end_node_ids = {}
+
+        # 如果某两个节点对应的合并分支节点id集合完全相同，则认为是重复/重叠节点，添加到duplicate_end_node_ids中
         for node_id, branch_node_ids in merge_branch_node_ids.items():
             for node_id2, branch_node_ids2 in merge_branch_node_ids.items():
                 if node_id != node_id2 and set(branch_node_ids) == set(branch_node_ids2):
@@ -610,6 +646,7 @@ class Graph(BaseModel):
                     ) not in duplicate_end_node_ids:
                         duplicate_end_node_ids[(node_id, node_id2)] = branch_node_ids
 
+        # 删除靠后的节点
         for (node_id, node_id2), branch_node_ids in duplicate_end_node_ids.items():
             # check which node is after
             if cls._is_node2_after_node1(node1_id=node_id, node2_id=node_id2, edge_mapping=edge_mapping):
@@ -619,6 +656,7 @@ class Graph(BaseModel):
                 if node_id in merge_branch_node_ids and node_id2 in merge_branch_node_ids:
                     del merge_branch_node_ids[node_id]
 
+        # 将节点分配到各自的分支(分支起始节点id -> 合并节点id)
         branches_merge_node_ids: dict[str, str] = {}
         for node_id, branch_node_ids in merge_branch_node_ids.items():
             if len(branch_node_ids) <= 1:
@@ -630,14 +668,18 @@ class Graph(BaseModel):
 
                 branches_merge_node_ids[branch_node_id] = node_id
 
+        # 为每个分支编译节点
+        # 上面的过程得到了最终的合并节点初始id，以及每个分支的合并节点id、每个分支的路径所有节点的id
         in_branch_node_ids: dict[str, list[str]] = {}
         for branch_node_id, node_ids in routes_node_ids.items():
+            # 对于每个分支，如果不存在合并点，则包含所有收集的节点， 就是一条单独的路径
             in_branch_node_ids[branch_node_id] = []
             if branch_node_id not in branches_merge_node_ids:
                 # all node ids in current branch is in this thread
                 in_branch_node_ids[branch_node_id].append(branch_node_id)
                 in_branch_node_ids[branch_node_id].extend(node_ids)
             else:
+                # 如果存在合并点，从分支起始节点遍历到合并点(不包括)。
                 merge_node_id = branches_merge_node_ids[branch_node_id]
                 if merge_node_id != branch_node_id:
                     in_branch_node_ids[branch_node_id].append(branch_node_id)
@@ -658,6 +700,7 @@ class Graph(BaseModel):
     ) -> None:
         """
         Recursively fetch route
+        从给定的起始节点开始，收集该节点可到达的所有节点ID。
         """
         if start_node_id not in edge_mapping:
             return
@@ -677,7 +720,9 @@ class Graph(BaseModel):
     ) -> bool:
         """
         Recursively check if the node is in the routes
+        该方法检查节点是否是所有分支中的公共点，表明这是一个有效的合并点。
         """
+        # 如果是StartNode，就不是有效的合并点，因为不在reverse_edge_mapping中，
         if start_node_id not in reverse_edge_mapping:
             return False
 
